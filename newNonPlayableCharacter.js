f301917484ad93661b5cfedc58b7ed8cf4489bc3
Nathan Towe <nathanwtowe89@gmail.com>
2023-09-11 23:27:00 -0400

    finished practice assignment

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -22,28 +22,52 @@ function newNonPlayableCharacter(x, y) {
     }
 
     setInterval(moveCharacter, 1)
-
-    function walkEast() {
+    
+    function walkEast(time){
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        return new Promise(function(resolve, reject){
+            setTimeout(() =>{
+                stop()
+                resolve()
+            }, time)
+        })
     }
 
-    function walkNorth() {
+    function walkNorth(time) {
         direction = 'north'
         element.src = `./assets/red-character/north.gif`
+        return new Promise(function(resolve, reject){
+            setTimeout(() =>{
+                stop()
+                resolve()
+            }, time)
+        })
     }
 
-    function walkWest() {
+    function walkWest(time) {
         direction = 'west'
         element.src = `./assets/red-character/west.gif`
+        return new Promise(function(resolve, reject){
+            setTimeout(() =>{
+                stop()
+                resolve()
+            }, time)
+        })
     }
 
-    function walkSouth() {
+    function walkSouth(time) {
         direction = 'south'
         element.src = `./assets/red-character/south.gif`
+        return new Promise(function(resolve, reject){
+            setTimeout(() =>{
+                stop()
+                resolve()
+            }, time)
+        })
     }
 
-    function stop() {
+    function stop(time) {
         direction = null
         element.src = `./assets/red-character/static.gif`
     }
